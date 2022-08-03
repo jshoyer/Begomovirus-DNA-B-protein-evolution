c76f36b22acc4dbb48e653f6278496b0251be510
dd886 <78744857+dd886@users.noreply.github.com>
2022-08-03 12:57:01 -0400

    Protein diversity code is written in python. BLOSUM50 matrix is used for protein diversity. Tanglegram is written in R

---
+++ Tanglegram_untangled.R
@@ -0,0 +1,44 @@
+install.packages("installr")
+library(installr)
+#updateR()
+
+#install.packages("Rtools", repos="https://cran.rstudio.com/bin/windows/Rtools/", quiet=TRUE)
+
+install.packages("ape")
+install.packages("ape",repos="https://cloud.r-project.org",quiet=TRUE)
+install.packages("dendextend")
+install.packages("viridis")
+install.packages("dplyr")
+install.packages("phylogram")
+install.packages("phangorn",repos="https://cloud.r-project.org",quiet=TRUE)
+install.packages("phytools",repos="https://cloud.r-project.org",quiet=TRUE)
+
+library(ape)
+library(phytools)
+library(dendextend)
+library(viridis)
+library(dplyr)
+library(phylogram)
+
+tree1 <- read.tree(file = "file name - newick format")
+#tree1 <- midpoint.root(tree1)
+tree2 <- read.tree(file = "file name")
+plot(tree2)
+#tree2 <- midpoint.root(tree2)
+tree1 <- compute.brlen(tree1)
+tree2 <- compute.brlen(tree2)
+tree1 <- as.dendrogram(tree1)
+tree1
+tree2 <- as.dendrogram(tree2)
+tree2
+
+#Had to detach "phytools" because it also has an untangle function
+detach("package:phytools")
+
+dndlist <- dendextend::dendlist(tree1, tree2)%>% untangle(method= "step2side")
+dendextend::tanglegram(dndlist, fast = TRUE, margin_inner = 1.8, lab.cex = 0.3, lwd = 
+                         0.5, edge.lwd = 0.5, type = "r")
+
+#print tanglegram
+dev.copy(pdf, 'Tanglegram_untangled.pdf', width = 10, height = 11)
+dev.off()
